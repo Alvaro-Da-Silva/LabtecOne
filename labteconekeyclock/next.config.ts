835eb04a27a2145dbb00df63ad659b136b1a7b4b
bristotartur <arturdarosabristot@gmail.com>
2025-11-28 14:25:35 -0300

    Configuração do projeto para deploy

--- labteconekeyclock/next.config.ts
+++ labteconekeyclock/next.config.ts
@@ -4,6 +4,8 @@ const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
 const nextConfig: NextConfig = {
     distDir: "build",
     basePath: baseUrl,
+
+    output: "standalone"
 };
 
 console.log("Configs Next.js carregadas")
