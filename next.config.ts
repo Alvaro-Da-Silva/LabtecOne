e90f79609dd8a7d191beacdcb104936a4cb40051
bristotartur <arturdarosabristot@gmail.com>
2025-12-09 16:06:24 -0300

    Ajustes no Dockerfile

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,7 @@ import type { NextConfig } from "next";
 const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
 
 const nextConfig: NextConfig = {
-    distDir: "build",
     basePath: baseUrl,
-    assetPrefix: baseUrl,
-
     output: "standalone"
 };
 
